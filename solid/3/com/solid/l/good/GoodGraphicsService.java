1b099b15e7e7b4b5948a28d686f570a1e8b53606
Petrenciuc Amelia <amelia.petrenciuc@gmail.com>
2024-10-17 14:18:15 +0300

    Added new classes

---
+++ solid/3/com/solid/l/good/GoodGraphicsService.java
@@ -0,0 +1,9 @@
+package com.solid.l.good;
+
+public class GoodGraphicsService {
+
+    public void checkForArea(Shape shape){
+        System.out.println("The area of this shape is: "+shape.getArea());
+    }
+
+}
